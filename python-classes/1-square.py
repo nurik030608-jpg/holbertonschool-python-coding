1acb74f51e33a61588d8db0c2ddb9709a52b89b0
nurik030608-jpg <nurik030608@gmail.com>
2025-12-10 16:36:48 +0400

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,10 +3,16 @@
 This module contains the definition of the Square class. 
 It meets all requirements for task 1, including size validation. 
 """
+
+
 class Square:
+    """
+    Defines a square by its size and handles initialization with validation. 
+    This is the required class docstring.
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise TypeError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
