8b1793e3394943b9bc6263184a080b113c4e39e0
nurik030608-jpg <nurik030608@gmail.com>
2025-12-10 16:10:43 +0400

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,11 +7,5 @@ class Square:
     def __init__(self, size):
         """
         Initializes a new Square instance.
-
-        Args:
-            size: The size of one side of the square. 
-                  (No type or value verification is performed.)
         """
-        # Private instance attribute '__size' is set here. 
-        # The double underscore invokes name mangling in Python.
         self.__size = size
