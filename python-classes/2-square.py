a4a2d146cbc27f653ef4c8f296d8916b1234d0fa
nurik030608-jpg <nurik030608@gmail.com>
2025-12-10 16:49:44 +0400

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -16,5 +16,5 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    def area(self)
+    def area(self):
     return self.__size ** 2
